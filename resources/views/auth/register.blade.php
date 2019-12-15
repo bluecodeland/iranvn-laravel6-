37fc35c9d89545a2b87e2a118153334378238eb6
bluecodeland <mhtaeb@gmail.com>
2019-12-15 20:11:21 +0330

    ui changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,17 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-md-5 pt-md-5">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card blue-g fff">
+                <div class="card-header text-right">ثبت نام</div>
 
-                <div class="card-body">
+                <div class="card-body offset-md-2">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -23,10 +22,11 @@
                                     </span>
                                 @enderror
                             </div>
+                            <label for="name" class="col-md-4 col-form-label text-md-left">نام و نام خانوادگی</label>
+
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -37,10 +37,11 @@
                                     </span>
                                 @enderror
                             </div>
+                            <label for="email" class="col-md-4 col-form-label text-md-left">پست الکترونیکی</label>
+
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -51,20 +52,23 @@
                                     </span>
                                 @enderror
                             </div>
+                            <label for="password" class="col-md-4 col-form-label text-md-left">رمز عبور</label>
+
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-left">تکرار رمز عبور</label>
+
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                   ثبت
                                 </button>
                             </div>
                         </div>
