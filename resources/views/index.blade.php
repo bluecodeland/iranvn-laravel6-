2b70eae0afd008e77f0ac083ddea9cea0d2a66fb
bluecodeland <mhtaeb@gmail.com>
2020-02-16 12:00:51 +0330

    some fixes and update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,21 +24,21 @@
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="https://picsum.photos/1280/400" class="d-block w-100" alt="...">
+                <img src="https://picsum.photos/id/210/1280/400" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>First slide label</h5>
                   <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://picsum.photos/1280/400" class="d-block w-100" alt="...">
+                <img src="https://picsum.photos/id/220/1280/400" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Second slide label</h5>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://picsum.photos/1280/400" class="d-block w-100" alt="...">
+                <img src="https://picsum.photos/id/230/1280/400" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>Third slide label</h5>
                   <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
