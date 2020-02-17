4c120f09c2374e8569c87f83f8c1e1c348e7f4a1
bluecodeland <mhtaeb@gmail.com>
2020-02-17 12:29:27 +0330

    some changes
    1.fix banner section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
     {{-- Header Start --}}
     <div class="headerbg mt-n4 pb-2">
     <div class="container pt-5">
-        <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+        <div id="carouselExampleCaptions" class="carousel slide carousel-fade slider-border" data-ride="carousel">
             <ol class="carousel-indicators">
               <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
               <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
@@ -38,10 +38,10 @@
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://picsum.photos/id/230/1280/400" class="d-block w-100" alt="...">
+                <img src="image/advice-banner.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <h5>Third slide label</h5>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  <h5></h5>
+                  <p>ما با ارائه راهنمایی های لازم به شما، درصد موفقیت شما در فضای مجازی را به طور ویژه ای بالا می بریم</p>
                 </div>
               </div>
             </div>
@@ -98,40 +98,7 @@
 {{--  ---- ----- -----  --}}
 
 {{-- Services --}}
-<div class="specials mb-5 bluebell ">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 py-5">
-                <h1 class="text-center">خدمات ما</h1>
-            </div>
-            <div class="col-md-4 ">
-                <div class="card animated fadeInUp mb-5">
-                    <img src="image/24-7.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <p class="card-text text-right text-justify">خدمات و پشتیبانی ما 24 ساعته و 7 روزه هفته برای شما فعال است</p>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-md-4 mb-5 animated fadeInUp">
-                <div class="card">
-                    <img src="image/bestoffer.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <p class="card-text text-right text-justify">بهترین پیشنهاد ها و مناسب ترین قیمت ها با حفظ بهترین کیفیت کار در انتظار شماست</p>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-md-4  mb-md-5 animated fadeInUp">
-                <div class="card">
-                    <img src="image/fast.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <p class="card-text text-right text-justify">ما در سریع ترین زمان ممکن خدمات را به شما مــی رسانیم و در عین حال بهترین کیفیت ممکن را خدمت شما ارائه خواهیم کرد</p>
-                    </div>
-                  </div>
-            </div>
-        </div>
-    </div>
 
-</div>
 
 {{-- Services End --}}
 {{--  ---- ----- -----  --}}
