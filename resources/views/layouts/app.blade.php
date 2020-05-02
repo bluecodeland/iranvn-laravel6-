3539cc344d55c75c6ea76941cf4299dc794bd368
bluecodeland <mhtaeb@gmail.com>
2020-05-02 16:04:37 +0430

    update css and deps

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,8 +34,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <!-- Icon font -->
-    {{-- <link rel="stylesheet" href="https://d1azc1qln24ryf.cloudfront.net/114779/Socicon/style-cf.css?u8vidh"> --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"></head>
 
     {{-- <!-- Modernizr JS -->
